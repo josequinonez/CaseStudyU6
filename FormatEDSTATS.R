d6cc9b27b4f70aef1cc2db41f92e84d17169c040
josequinonez <jose.quinonez.lopez@hotmail.com>
2016-06-19 10:51:53 -0500

    Delete FormatEDSTATS.R
    This file was moved to Code dir

--- FormatEDSTATS.R
+++
@@ -1,8 +0,0 @@
-## Asssign initial working vector
-edstats1 <- edstats
-
-## Assign names to columns according to content of row 1
-names(edstats1) <- gsub(" ", "_", edstats1[1,])
-
-## Eliminate the first row because it corresponds to column names
-edstats2 <- edstats1[2:235,]
